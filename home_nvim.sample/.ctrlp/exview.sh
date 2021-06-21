755893611a14c1492d39cfc089fcb0450b2f54ea
Ta-To <61908291+Ta-To@users.noreply.github.com>
2021-06-21 12:09:17 +0900

    Add ctrlp-launcherの設定をリポジトリに追加

---
+++ home_nvim.sample/.ctrlp/exview.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Phoenix ビュー移動
+# $1: 現ファイルパス (%)
+# $2: カレントディレクトリ (cwd)
+
+# FILENAME=`echo $1 | sed 's/\.[^\/]*$//'`
+EXT_LIST=(".html.slimleex" ".html.slimeex" ".html.leex" ".html.eex")
+
+#   同一フォルダ内
+for extname in ${EXT_LIST[@]}; do
+  filepath="${FILENAME}${extname}"
+  if [ -e ${filepath} ]; then
+    echo ${filepath}
+    exit
+  fi
+done
+
+#   最終的にファイルが見つからない場合は慣習的に .html.slimleex
+echo "${FILENAME}${EXT_LIST[0]}"
